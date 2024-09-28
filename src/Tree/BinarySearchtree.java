d34c29f6d390b9e8d2c714d93ae4a6ad8ceeadc9
AnHNT <AnHNT@admin>
2024-09-29 03:24:37 +0700

    CSD

---
+++ src/Tree/BinarySearchtree.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tree;
+
+/**
+ *
+ * @author se180663
+ */
+public class BinarySearchtree {
+    Node root;
+
+    public void insert(Node node) {}
+        private Node insertHelper(Node root, Node node){
+            return null;
+        }
+        public void display(){
+    }
+        private void displayHelper(Node root){}
+        public boolean search(int data){
+        return false;
+            
+        }
+         public boolean searchHelper(Node root,int data){
+        return false;
+            
+        }
+        public void remove(int data){
+            
+        }
+        public Node removeHelper(Node root,int data){
+            return null;
+        }
+}
